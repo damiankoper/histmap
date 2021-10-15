809c4a172ace444484b147eeec8b937ede9ba05c
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-10-15 12:06:42 +0200

    Map page setup & added search component

---
+++ packages/front/src/components/SearchInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="search-container">
+    <img src="../assets/images/burger.svg" alt="burger menu" />
+    <el-input
+      v-model="input"
+      placeholder="Szukaj miejsca"
+      class="search-input"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    return {
+      input: ref(""),
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+@import "../assets/css/_variables.scss";
+
+.search-container {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  background-color: white;
+  border-radius: 10px;
+  width: 300px;
+  border: 1px solid $a-grey;
+  padding: 2px 30px 2px 10px;
+
+  img {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+  }
+}
+
+.search-input {
+  margin-left: 10px;
+  input {
+    height: 30px;
+    border: none;
+  }
+}
+</style>
--- packages/front/src/views/Map.vue
+++ packages/front/src/views/Map.vue
@@ -1,5 +1,41 @@
+<template>
+  <div class="container">
+    <div class="search-wrapper">
+      <SearchInput />
+    </div>
+    <img src="../assets/images/map_placeholder.png" alt="placeholder" />
+  </div>
+  <Footer />
+</template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
+import Footer from "../components/Footer.vue";
+import SearchInput from "../components/SearchInput.vue";
 
-export default defineComponent({});
+export default defineComponent({
+  components: { Footer, SearchInput },
+  setup() {
+    return {};
+  },
+});
 </script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.search-wrapper {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+</style>
