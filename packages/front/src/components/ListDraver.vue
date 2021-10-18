5e8e82a537830467bdaf446daf3b386cc997b487
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-10-18 14:33:41 +0200

    Added ListDrawer with its cards

--- packages/front/src/components/ListDraver.vue
+++ packages/front/src/components/ListDraver.vue
@@ -1,10 +1,63 @@
 <template>
   <el-drawer
     v-model="store.state.showListDialog"
-    title="Poniżej wyświetlono wyniki, znajdujące się na wybranym obszarze."
+    title="HISTmap"
     direction="ltr"
   >
-    <span>test test</span>
+    <span class="subtitle"
+      >Poniżej wyświetlono wyniki, znajdujące się na wybranym obszarze.</span
+    >
+    <!-- TODO make height somehow work -->
+    <el-scrollbar always height="50%" wrap-class="lol">
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <h3>Architektura komputerów</h3>
+        <span class="card-info">Janusz Biernat</span>
+        <span class="card-info">Wrocław 1998</span>
+        <span class="card-info">ISBN 12389743829798234</span>
+      </el-card>
+    </el-scrollbar>
   </el-drawer>
 </template>
 
@@ -20,3 +73,22 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.card {
+  margin: 10px 10px 10px 0;
+}
+h3 {
+  margin: 0;
+}
+.card-info {
+  display: block;
+  margin-top: 10px;
+}
+
+.subtitle {
+  display: block;
+  margin-top: -20px;
+  margin-bottom: 20px;
+}
+</style>
