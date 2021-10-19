5e76bc4fbe267c1b7deda8d1cc87419e20ddf5f6
damiankoper <kopernickk@gmail.com>
2021-10-19 10:29:48 +0200

    chore: types

---
+++ packages/pre-processor/types/types.d.ts
@@ -0,0 +1,33 @@
+export interface Data {
+    publications: Publication[]
+    preTiles: PreTile[]
+    stats: TileStats[]
+}
+
+export interface TileStats {
+    t: number
+    z: number
+    max: number
+}
+
+export interface Publication {
+    id: number
+    title: string
+    author: string
+    places: string[]
+    year: number
+}
+
+export interface PreTile {
+    x: number
+    y: number
+    z: number
+    t: number
+    points: Point[]
+}
+
+export interface Point {
+    x: number
+    y: number
+    publications: number[]
+}
