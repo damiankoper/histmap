f4e7d60369e44165c0e9e6d75a9d3e8672972635
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-12-08 16:00:55 +0100

    Removed unused module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TilesModule } from './tiles/tiles.module';
@@ -6,7 +6,7 @@ import { AreaModule } from './area/area.module';
 import { MathModule } from './math/math.module';
 
 @Module({
-  imports: [TilesModule, AreaModule, CacheModule.register(), MathModule],
+  imports: [TilesModule, AreaModule, MathModule],
   controllers: [AppController],
   providers: [AppService],
 })
