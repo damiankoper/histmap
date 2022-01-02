937ad7ddf75da12a3d965c91c1edc0db9a8a22cd
damiankoper <kopernickk@gmail.com>
2022-01-02 21:57:53 +0100

    fix: compress

--- packages/pre-processor/src/compress.js
+++ packages/pre-processor/src/compress.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
-const lz = require("lz-string");
+const {gzip, ungzip} = require('node-gzip')
 
 const DATA_DIR = "../../data/";
 
 console.log("Reading data.json...");
-const dataString = fs.readFileSync(`${DATA_DIR}data.json`, {encoding: "utf-8"});
+const dataString = fs.readFileSync(`${DATA_DIR}data.json`, { encoding: "utf-8" });
 console.log(`data.json is ${dataString.length} characters long`);
 console.log("Compressing...");
-const dataCompressed = lz.compressToUint8Array(dataString);
-console.log("Saving compressed data...");
-fs.writeFileSync(`${DATA_DIR}data`, dataCompressed);
+
+(async () => {
+    const dataCompressed = await gzip(dataString)
+    console.log("Saving compressed data...");
+    fs.writeFileSync(`${DATA_DIR}data`, dataCompressed);
+})()
+
