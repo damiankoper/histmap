24506b1ef0f2d0ae202b0ea932e95704767dd4ea
damiankoper <kopernickk@gmail.com>
2022-01-02 22:28:35 +0100

    fix: preprocessor types

--- packages/pre-processor/types/types.d.ts
+++ packages/pre-processor/types/types.d.ts
@@ -1,48 +1,53 @@
 export interface Data {
-    publications: Publication[]
-    areas: Area[]
-    areaStats: AreaStats[]
-    preTiles: PreTile[]
-    stats: TileStats[]
+  publications: Publication[]
+  areas: Area[]
+  areaStats: AreaStats[]
+  preTiles: PreTile[]
+  stats: TileStats[]
 }
 
 export interface Publication {
-    id: number
-    title: string
-    author: string
-    publicationPlace: string
-    year: number
+  id: number
+  title: string
+  author: string
+  publicationPlace: string
+  year: number
+}
+
+export interface TilePositionCoords {
+  x: number
+  y: number
+}
+
+export interface TileMetaCoords {
+  z: number
+  t: number
+}
+
+export interface TileCoords extends TileMetaCoords, TilePositionCoords { }
+
+export interface TileStats extends TileMetaCoords {
+  max: number
+}
+
+export interface AreaStats extends TileMetaCoords {
+  id: number,
+  pointCount: number
 }
 
 export interface Area {
-    id: number
-    t: number
-    publications: number[]
+  id: number,
+  t: number,
+  publications: number[]
 }
 
-export interface AreaStats {
-    id: number
-    z: number
-    pointCount: number
-}
-
-export interface PreTile {
-    x: number
-    y: number
-    z: number
-    t: number
-    points: Point[]
+export interface PreTile extends TileCoords {
+  points: Point[]
 }
 
 export interface Point {
-    x: number
-    y: number
-    areas: number[]
-    publications: number[]
-}
-
-export interface TileStats {
-    t: number
-    z: number
-    max: number
+  x: number
+  y: number
+  areas: number[],
+  publications: number[]
 }
