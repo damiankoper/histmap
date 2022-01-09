b93ff954e8ac6e4fb8b19134ee4f071547351bc6
damiankoper <kopernickk@gmail.com>
2022-01-09 14:26:59 +0100

    feat: minimum data interface

--- packages/pre-processor/types/types.d.ts
+++ packages/pre-processor/types/types.d.ts
@@ -6,12 +6,27 @@ export interface Data {
   stats: TileStats[]
 }
 
+export interface DataDict {
+  publications: PublicationDict[]
+  areas: AreaDict[]
+  areaStats: AreaStatsDict[]
+  preTiles: PreTileDict[]
+  stats: TileStatsDict[]
+}
+
 export interface Publication {
   id: number
   title: string
   author: string
   publicationPlace: string
   year: number
+}
+export interface PublicationDict {
+  i: number
+  t: string
+  a: string
+  p: string
+  y: number
 }
 
 export interface TilePositionCoords {
@@ -30,10 +45,20 @@ export interface TileStats extends TileMetaCoords {
   max: number
 }
 
+export interface TileStatsDict extends TileMetaCoords {
+  m: number
+}
+
 export interface AreaStats {
   id: number,
   z: number
   pointCount: number
+}
+
+export interface AreaStatsDict {
+  i: number,
+  z: number
+  p: number
 }
 
 export interface Area {
@@ -42,13 +67,32 @@ export interface Area {
   publications: number[]
 }
 
+export interface AreaDict {
+  i: number,
+  t: number,
+  p: number[]
+}
+
 export interface PreTile extends TileCoords {
   points: Point[]
 }
+
+export interface PreTileDict extends TileCoords {
+  p: PointDict[]
+}
+
 
 export interface Point {
   x: number
   y: number
   areas: number[],
   publications: number[]
+}
+
+// !If a or p is empty, it is not defined at all
+export interface PointDict {
+  x: number
+  y: number
+  a?: number[],
+  p?: number[]
 }
